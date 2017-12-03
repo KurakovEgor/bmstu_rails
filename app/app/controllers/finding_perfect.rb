900ae4b984b0ee5a52d18783ebcdb92a28799cea
Egor Kurakov <egor@live.ru>
2017-12-04 02:46:44 +0300

    add all functions

--- app/app/controllers/finding_perfect.rb
+++ app/app/controllers/finding_perfect.rb
@@ -7,3 +7,22 @@ end
 def perfect?(number)
   get_dividers(number).inject(0) { |result, elem| result + elem } == number
 end
+
+def perfect_seqs(seq_of_numbers)
+  result_sequences = [[]]
+  prev_is_perfect = false
+  seq_of_numbers.each { |i|
+    if perfect? i
+      result_sequences.last.push(i)
+      prev_is_perfect = true
+    elsif prev_is_perfect
+      result_sequences.push([])
+      prev_is_perfect = false
+    end
+  }
+  result_sequences.select(&:any?)
+end
+
+def longest_seq(seq_of_seq)
+  seq_of_seq.max_by(&:length)
+end
--- app/app/controllers/perfect_numbers_controller.rb
+++ app/app/controllers/perfect_numbers_controller.rb
@@ -1,23 +1,17 @@
 require 'finding_perfect'
 
 class PerfectNumbersController < ApplicationController
-  #def check_sequence
-  #  redirect_to action: 'check_sequence_input'
-  #end
-  
-  def check_sequence_input
-  end
 
-  #def send_n
-    #render plain: params[:n].inspect
-  #end
+  def check_sequence_input; end
 
   def check_sequence_input_array
     @length_of_array = params[:n].to_i
   end
 
   def get_result
-    @numbers = []
-    params[:input_numbers].each { |k, v| @numbers.push(v.to_i) }
+    numbers = []
+    params[:input_numbers].each { |_, v| numbers.push(v.to_i) }
+    @sequences = perfect_seqs(numbers)
+    @longest_seq = longest_seq(@sequences)
   end
 end
